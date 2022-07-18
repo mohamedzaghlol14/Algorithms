e26a221ad4de62188082c78cac2a5ed0203fa41e
Mohamed Zaghlol <72021541+mohamedzaghlol14@users.noreply.github.com>
2022-07-18 03:36:48 +0200

    Add files via upload

---
+++ FindSmalest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class FindSmalest
+    {
+        public static int Sort(int[] myarray)
+        {
+            int minidx = myarray[0];
+
+            foreach (int item in myarray)
+            {
+                if (minidx < item)
+                    minidx = item;
+                    myarray[0] = minidx;
+
+            }
+
+            return minidx;
+        }
+
+      
+    }
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algorithms
+
+{
+    public class SelectionSort
+    {
+        public static int[] sort(int[] myarray)
+        {
+
+            int no = myarray.Length;
+            for(int i = 0; i < no - 1 ; i++)
+            {
+                int minidx = i;
+                int minvalue = myarray[i];
+
+                for(int j = i + 1; j < no ; j++)
+                {
+                    if(myarray[j] < minvalue) 
+                    {
+                        minidx = j; 
+                        minvalue = myarray[j]; 
+                    }
+
+                }
+                myarray[minidx] = myarray[i];
+                myarray[i] = minvalue;
+            }
+
+            return myarray;
+        }
+
+    }
+
+}
